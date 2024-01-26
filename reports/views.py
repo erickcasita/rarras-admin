abf57dfa574cad1bbdd98e033a94f3acba6ef5bd
Yosimar Zahid Aquino Sosa <yosimaraquinos@proton.me>
2024-01-26 15:38:51 -0600

    Correccion de error gramatical y listado de almacenes y categorias en vista de reporte de exitencias

--- reports/views.py
+++ reports/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from products.models import Categories
+from warehouses.models import WareHouses
 
 # Create your views here.
 
 @login_required
 def reportstock (request):
     if request.method ==  'GET':
-        return render(request, 'reportstock.html')
+        categories = Categories.objects.filter(visible=True)
+        werehouses = WareHouses.objects.filter(visible=True)
+        return render(request, 'reportstock.html', {
+            'categories': categories,
+            'werehouses': werehouses
+        })
     else:
         pass
