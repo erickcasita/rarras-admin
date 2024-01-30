70d67fcbbec11a6118100f515539eec40b1d6d6b
Erick Casanova <casa.erickmr@gmail.com>
2024-01-29 18:02:45 -0600

    Views reporte movimientos de almacen

--- reports/views.py
+++ reports/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from products.models import Categories
-from warehouses.models import WareHouses,WereHouseStock,WereHouseMovement
+from warehouses.models import WareHouses,WereHouseStock, WereHouseMovementDetails, WareHouseConcept,TypeMovement
 from products.models import Categories
 from django.db.models import Sum
 from django.contrib import messages
@@ -63,4 +63,41 @@ def reportmovement (request):
             'formtypemovement': AddWareHouseConcept,
         })
     else:
-        pass
+        concepid = request.POST.get('concept')
+        typemovement = request.POST.get('typemovement')
+        idwerehouse = request.POST.get('werehouse')
+        initdate = request.POST.get('date_init')
+        finaldate = request.POST.get('date_finish')
+        user = request.user.username
+        datenow = datetime.now()
+        movement = WereHouseMovementDetails.objects.all().filter(werehousemovement__created__range=(initdate, finaldate), 
+                                                                 werehousemovement__werehouseconcept_id = concepid,
+                                                                 werehousemovement__typemovement_id = typemovement)
+        totales = WereHouseMovementDetails.objects.all().filter(werehousemovement__created__range=(initdate, finaldate), 
+                                                                 werehousemovement__werehouseconcept_id = concepid,
+                                                                 werehousemovement__typemovement_id = typemovement).aggregate(Sum('canmov'))
+        
+        #test = WereHouseMovementDetails.objects.all().filter(werehousemovement__created__range=(initdate, finaldate), 
+        #                                                        werehousemovement__typemovement_id = typemovement)
+        concept = WareHouseConcept.objects.get(visible = True , pk = concepid)
+        tmovement = TypeMovement.objects.get(visible = True , pk = typemovement)
+        werehouses = WareHouses.objects.get(visible = True , pk = idwerehouse)
+        #folios = [20,24,25,26]
+        #print(i.werehousemovement.id , i.werehousemovement.typemovement.name, i.werehousemovement.werehouseconcept.title, i.product.name, i.canmov)
+        context = {'movement': movement, 'concept': concept, 'tmovement': tmovement, 'werehouses': werehouses,
+                   'initdate': initdate, 'finaldate': finaldate, 'user': user, 'datenow': datenow, 'totales': totales}
+    
+        # Create a Django response object, and specify content_type as pdf
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+        # find the template and render it.
+        template = get_template('core/reportmovementsform.html')
+        html = template.render(context)
+
+        # create a pdf
+        pisa_status = pisa.CreatePDF(
+        html, dest=response)
+        # if error then show some funny view
+        if pisa_status.err:
+            return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return response
