b2e09b012f2b7d5ef20fa0e557e8289a5e51c1b6
Yosimar Zahid Aquino Sosa <yosimaraquinos@proton.me>
2024-01-30 18:21:59 -0600

    Vista dashboard agregada

---
+++ home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from products.models import Products
+from store.models import Store
+from warehouses.models import WareHouses, WereHouseStock
+from django.db.models import Count,F
+
+# Create your views here.
+@login_required
+def dashboard (request):
+    if request.method ==  'GET':
+        products = Products.objects.filter(visible=True).count()
+        stores = Store.objects.count()
+        werehouses = WareHouses.objects.filter(visible=True).count()
+        werehouseStock = WereHouseStock.objects.values('werehouse').annotate(stock=Count('product'), title=F('werehouse__title')).annotate().order_by('werehouse')
+        return render(request, 'dashboard.html',
+            {
+                'products': products,
+                'stores': stores,
+                'werehouses': werehouses,
+                'werehouseStock': werehouseStock
+            })
+    else:
+        pass
